8f0fa4394b9b6496b7667b95609c71040db28f3f
Sanjay Kumar <skkhatri948@gmail.com>
2021-08-11 01:03:02 +0500

    Todo List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponent/todos/todos.component';
+import { TodoItemComponent } from './MyComponent/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponent/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
